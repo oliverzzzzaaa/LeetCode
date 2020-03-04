72cc479bd2798e8818a5383bbc0492b4c115cd0d
Oliver <oliverzzzzaaa@gmail.com>
2020-03-04 11:15:14 -0800

    completed 422

---
+++ medium/442.js
@@ -0,0 +1,15 @@
+var findDuplicates = function(nums) {
+    let hash = {};
+    let i = 0;
+    let unique = 0;
+    while (i < nums.length) {
+        if (hash[nums[i]] === 1) {
+            nums[unique] = nums[i]
+            unique+=1;
+        } else if (!hash[nums[i]])  {
+            hash[nums[i]] = 1;
+        }
+        i+=1;
+    }
+    nums.slice(0, unique)
+};
