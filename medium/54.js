47cb3f0b893733d5377a4cd88bfdbe2a3f407de4
Oliver <oliverzzzzaaa@gmail.com>
2020-03-26 23:05:16 -0700

    finished 54 and spiral2

--- medium/54.js
+++ medium/54.js
@@ -15,3 +15,63 @@ var spiralOrder = function(matrix) {
     }
     return res;
 };
+
+
+var spiral2 = function(n) {
+    if (n < 1) {return []}
+    let ret = [];
+    for (let i = 0; i < n; i++) {
+        let row = [];
+        for (let j = 0; j < n; j++) {
+            row.push(null)
+        }
+        ret.push(row)
+    }
+    let count = 1;
+    for (let i = 0; i < n; i++) {
+        ret[0][i] = count;
+        count++;
+    }
+    let x = n-1;
+    let y = 0;
+    let dirs = [[1,0],[0,1],[-1,0],[0,-1]]
+    let dir = 1;
+    let layer = n-1;
+    while (layer > 0) {
+        for (let i = 0; i < layer; i++) {
+            x += dirs[dir % 4][0]
+            y += dirs[dir % 4][1]
+            ret[y][x] = count;
+            count++;
+        }
+        dir++;
+        for (let i = 0; i < layer; i++) {
+            x += dirs[dir % 4][0]
+            y += dirs[dir % 4][1]
+            ret[y][x] = count;
+            count++;  
+        }
+        dir++;
+        layer--;
+        console.log(ret[2])
+    }
+    return ret;
+}
+
+// [1,2,3]
+// [8,9,4]
+// [7,6,5]
+
+// [01,02,03,04]
+// [12,13,14,05]
+// [11,16,15,06]
+// [10,09,08,07]
+
+// [01,02,03,04,05]
+// [16,17,18,19,06]
+// [15,24,25,20,07]
+// [14,23,22,21,08]
+// [13,12,11,10,09]
+
+
+console.log(spiral2(4))
