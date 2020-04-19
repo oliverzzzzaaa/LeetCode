b981c26048471c39a99e5dd839d102ca3621451d
Oliver <oliverzzzzaaa@gmail.com>
2020-04-19 13:14:35 -0700

    additional way to do 1209

--- medium/1209.js
+++ medium/1209.js
@@ -23,4 +23,47 @@ var removeDuplicates = function(s, k) {
     return dfs(s)
 };
 
+// var removeDuplicates = function(s, k) {
+//     let i = 0;
+//     let arr = new Array(s.length)
+//     arr.fill(true);
+//     let lastchar;
+//     let count = 0;
+//     while (i < s.length) {
+//         if (arr[i] && s[i] === lastchar) {
+//             count++;
+//             if (count === k) {
+//                 while (count > 0) {
+//                     if (s[i] === undefined) {break;}
+//                     if (s[i] === lastchar && arr[i]) {
+//                         arr[i] = false;
+//                         i--;
+//                         count--;
+//                     } else {
+//                         i--;
+//                     }
+//                 }
+//                 i = 0;
+//                 lastchar = null;
+//                 count = 0;
+//             } else {
+//                 i++;
+//             }
+//         } else if (arr[i] && s[i] !== lastchar){
+//             lastchar = s[i];
+//             count = 1;
+//             i++;
+//         } else {
+//             i++;
+//         }
+//     }
+//     let ans = [];
+//     for (let x = 0; x < arr.length; x++) {
+//         if (arr[x]) {
+//             ans.push(s[x])
+//         }
+//     }
+//     return ans.join("")
+// }
+
 console.log(removeDuplicates('deeedbbcccbdaa', 3))
