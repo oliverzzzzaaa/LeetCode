c5d45e857b21f34cdaa48e2b5e34143b1138c801
Oliver <oliverzzzzaaa@gmail.com>
2020-04-22 19:06:19 -0700

    415

--- Easy/415.js
+++ Easy/415.js
@@ -1,29 +1,43 @@
+
+// var addStrings = function(num1, num2) {
+//     const dict = {
+//         '0': 0,
+//         '1': 1,
+//         '2': 2,
+//         '3': 3,
+//         '4': 4,
+//         '5': 5,
+//         '6': 6,
+//         '7': 7,
+//         '8': 8,
+//         '9': 9
+//     }
+
+//     let carry = 1
+//     let sum = 0;
+//     for (let i = num1.length-1; i >= 0; i--) {
+//         // console.log(sum)
+//         console.log(dict[(num1[i])] * carry)
+//         sum += (dict[(num1[i])] * carry)
+//         carry *= 10;
+//         console.log(sum)
+//     }
+//     carry = 1;
+//     for (let j = num2.length-1; j >= 0; j--) {
+//         sum += (dict[(num2[j])] * carry)
+//         carry *= 10;
+//     }
+//     return sum.toString()
+// };
+
 var addStrings = function(num1, num2) {
-    const dict = {
-        '0': 0,
-        '1': 1,
-        '2': 2,
-        '3': 3,
-        '4': 4,
-        '5': 5,
-        '6': 6,
-        '7': 7,
-        '8': 8,
-        '9': 9
-    }
-
-    let carry = 1
-    let sum = 0;
-    for (let i = num1.length-1; i >= 0; i--) {
-        sum += (dict[(num1[i])] * carry)
-        carry *= 10;
-    }
-    carry = 1;
-    for (let j = num2.length-1; j >= 0; j--) {
-        sum += (dict[(num2[j])] * carry)
-        carry *= 10;
-    }
     
-};
 
-console.log(addStrings('122', '61'))
+}
+
+
+console.log(addStrings(
+    "9333852702227987",
+      "85731737104263"))
+
+// console.log(9000000000000000 + 300000000000000)
