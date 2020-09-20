3143a058b2fe14455c61e309ad55a25445a3182c
Oliver <oliverzzzzaaa@gmail.com>
2020-09-19 17:15:59 -0700

    846 in js

---
+++ medium/1557.js
@@ -0,0 +1,3 @@
+var findSmallestSetOfVertices = function(n, edges) {
+    
+};
--- medium/846.js
+++ medium/846.js
@@ -1,16 +1,26 @@
 var isNStraightHand = function(hand, W) {
-    if (hand.length % W !== 0) return false
-    hand = hand.sort();
+    if (hand.length % W !== 0) return false;
     let h = {};
     hand.forEach(num => {
-        if (!h[num]) {
-            h[num] = 1;
-        } else {
-            h[num]++;
+        if (!h[num]) h[num] = 0;
+        h[num]++;
+    });
+    let keys = Object.keys(h).sort((a,b) => a-b);
+    console.log(h)
+    for (let i = 0; i < keys.length; i++) {
+        let start = parseInt(keys[i]);
+        while (h[start]) {
+            if (!h[start]) continue;
+            // console.log(h)
+            for (let j = 0; j < W; j++) {
+                if (!h[start]) return false;
+                h[start]--;
+                if (h[start] === 0) delete h[start];
+                start++;
+            }
+            start =parseInt(keys[i]);
         }
-    })
-    
-    return true
+    }
+    // console.log(h)
+    return Object.keys(h).length === 0;
 };
-
-console.log(isNStraightHand([1,2,3,6,2,3,4,7,8], 3))
